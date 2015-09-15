bf5a04ab13681a85d11093cc2ea247fed6288e26
Dmitry Sky <dmitry.sky@icloud.com>
2015-09-14 18:20:45 -0600

    find action for merchant complete

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -4,4 +4,12 @@ class Api::V1::MerchantsController < ApplicationController
   def show
     respond_with Merchant.find_by(id: params[:id])
   end
+
+  def find
+    if params[:name]
+      respond_with Merchant.find_by_name(params[:name])
+    elsif params[:id]
+      respond_with Merchant.find_by_id(params[:id])
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get "merchants/find", to: "merchants#find"
       resources :merchants, only: [:show]
+
       resources :customers, only: [:show]
       resources :invoices, only: [:show]
       resources :invoice_items, only: [:show]
