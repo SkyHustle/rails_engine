319a280a6bc8a6fe1bb52ecb9c063f86a1a10b77
Dmitry Sky <dmitry.sky@icloud.com>
2015-09-15 17:46:50 -0600

    adjust show controller test expectations

--- test/controllers/api/v1/items_controller_test.rb
+++ test/controllers/api/v1/items_controller_test.rb
@@ -7,9 +7,9 @@ class Api::V1::ItemsControllerTest < ActionController::TestCase
     item = JSON.parse(response.body, symbolize_names: true)
 
     assert_response :success
-    assert_equal "Item Autem Minima", item[:name]
-    assert_equal "Cumque consequuntur ad. Fuga tenetur illo molestias enim aut iste. Provident quo hic aut. Aut quidem voluptates dolores. Dolorem quae ab alias tempora.",        item[:description]
-    assert_equal "67076.0",               item[:unit_price]
-    assert_equal 1,                   item[:merchant_id]
+    assert_equal "Item Autem Minima",       item[:name]
+    assert_equal "Cumque consequuntur ad.", item[:description]
+    assert_equal "67076.0",                 item[:unit_price]
+    assert_equal 1,                         item[:merchant_id]
   end
 end
