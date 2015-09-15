df26d0aad5faae7db84a26879eddcbc47a0bda52
Dmitry Sky <dmitry.sky@icloud.com>
2015-09-15 11:33:23 -0600

    show action controller tests complete

--- test/controllers/api/v1/invoices_controller_test.rb
+++ test/controllers/api/v1/invoices_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class Api::V1::InvoicesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#show" do
+    get :show, format: :json, id: Invoice.first.id
+
+    invoice = JSON.parse(response.body, symbolize_names: true)
+
+    assert_response :success
+    assert_equal 1,         invoice[:customer_id]
+    assert_equal 76,        invoice[:merchant_id]
+    assert_equal "shipped", invoice[:status]
+  end
 end
--- test/controllers/api/v1/items_controller_test.rb
+++ test/controllers/api/v1/items_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class Api::V1::ItemsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#show" do
+    get :show, format: :json, id: Item.first.id
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    assert_response :success
+    assert_equal "Item Autem Minima", item[:name]
+    assert_equal "Cumque consequuntur ad. Fuga tenetur illo molestias enim aut iste. Provident quo hic aut. Aut quidem voluptates dolores. Dolorem quae ab alias tempora.",        item[:description]
+    assert_equal 67076,               item[:unit_price]
+    assert_equal 1,                   item[:merchant_id]
+  end
 end
