d6fdf11634fb8721d67307bba9593d722f661a9c
Dmitry Sky <dmitry.sky@icloud.com>
2015-09-16 16:11:36 -0600

    items controller tests

--- test/controllers/api/v1/items_controller_test.rb
+++ test/controllers/api/v1/items_controller_test.rb
@@ -11,4 +11,65 @@ class Api::V1::ItemsControllerTest < ActionController::TestCase
     assert_equal "Nihil autem sit odio",    item[:description]
     assert_equal "75107.0",                 item[:unit_price]
   end
+
+  test "#index" do
+    get :index, format: :json
+
+    items = JSON.parse(response.body, symbolize_names: true)
+    item  = items.first
+
+    assert_response :success
+    assert_equal 2,       items.count
+    assert_equal "Item Qui Esse", item[:name]
+  end
+
+  test "#find" do
+    get :find, format: :json, id: Item.first.id
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    assert_response :success
+    assert_equal "75107.0", item[:unit_price]
+  end
+
+  test "#find_all" do
+    get :find_all, format: :json, id: Item.first.id
+
+    items = JSON.parse(response.body, symbolize_names: true)
+    item  = items.last
+
+    assert_response :success
+    assert_equal 1,           items.count
+    assert_equal "75107.0",   item[:unit_price]
+  end
+
+  test "#random" do
+    get :random, format: :json
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    assert_response :success
+    assert_equal Hash,  merchant.class
+    assert_equal false, merchant.empty?
+  end
+
+  test "#invoice_items" do
+    get :invoice_items, format: :json, id: Item.first.id
+
+    invoice_items = JSON.parse(response.body, symbolize_names: true)
+    invoice_item  = invoice_items.first
+
+    assert_response :success
+    assert_equal Array, invoice_items.class
+  end
+
+  test "#merchant" do
+    get :merchant, format: :json, id: Item.first.id
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+    merchant  = merchants.first
+
+    assert_response :success
+    assert_equal Hash, merchants.class
+  end
 end
--- test/controllers/api/v1/merchants_controller_test.rb
+++ test/controllers/api/v1/merchants_controller_test.rb
@@ -9,4 +9,67 @@ class Api::V1::MerchantsControllerTest < ActionController::TestCase
     assert_response :success
     assert_equal "Microsoft", merchant[:name]
   end
+
+  test "#index" do
+    get :index, format: :json
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+    merchant  = merchants.first
+
+    assert_response :success
+    assert_equal 2,         merchants.count
+    assert_equal "Apple", merchant[:name]
+  end
+
+  test "#find" do
+    get :find, format: :json, id: Merchant.first.id
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    assert_response :success
+    assert_equal "Microsoft", merchant[:name]
+  end
+
+  test "#find_all" do
+    get :find_all, format: :json, id: Merchant.first.id
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+    merchant  = merchants.last
+
+    assert_response :success
+    assert_equal 1,           merchants.count
+    assert_equal "Microsoft", merchant[:name]
+  end
+
+  test "#random" do
+    get :random, format: :json
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    assert_response :success
+    assert_equal Hash,  merchant.class
+    assert_equal false, merchant.empty?
+  end
+
+  test "#merchant_items" do
+    get :merchant_items, format: :json, id: Merchant.first.id
+
+    items     = JSON.parse(response.body, symbolize_names: true)
+    item      = items.first
+
+    assert_response :success
+    assert_equal Array, items.class
+    assert_equal "Item Autem Minima", item[:name]
+  end
+
+  test "#merchant_invoices" do
+    get :merchant_invoices, format: :json, id: Merchant.first.id
+
+    invoices = JSON.parse(response.body, symbolize_names: true)
+    invoice  = invoices.first
+
+    assert_response :success
+    assert_equal Array, invoices.class
+    assert_equal "shipped", invoice[:status]
+  end
 end
