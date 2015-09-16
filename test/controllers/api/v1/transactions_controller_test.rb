d06dfd37671a4b44585f0ad3c4e040c9f0181a3b
Dmitry Sky <dmitry.sky@icloud.com>
2015-09-16 15:31:35 -0600

    transactions controller tests complete

--- test/controllers/api/v1/transactions_controller_test.rb
+++ test/controllers/api/v1/transactions_controller_test.rb
@@ -10,4 +10,54 @@ class Api::V1::TransactionsControllerTest < ActionController::TestCase
     assert_equal "4411510861233607", transaction[:credit_card_number]
     assert_equal "failed",           transaction[:result]
   end
+
+  test "#index" do
+    get :index, format: :json
+
+    transactions = JSON.parse(response.body, symbolize_names: true)
+    transaction  = transactions.first
+
+    assert_response :success
+    assert_equal 2,         transactions.count
+    assert_equal "success", transaction[:result]
+  end
+
+  test "#find" do
+    get :find, format: :json, invoice_id: Transaction.first.invoice_id
+
+    transaction = JSON.parse(response.body, symbolize_names: true)
+
+    assert_response :success
+  end
+
+  test "#find_all" do
+    get :find_all, format: :json, id: Transaction.first.id
+
+    transactions = JSON.parse(response.body, symbolize_names: true)
+    transaction  = transactions.last
+
+    assert_response :success
+    assert_equal 1,         transactions.count
+    assert_equal "failed", transaction[:result]
+  end
+
+  test "#random" do
+    get :random, format: :json
+
+    invoice = JSON.parse(response.body, symbolize_names: true)
+
+    assert_response :success
+    assert_equal Hash,  invoice.class
+    assert_equal false, invoice.empty?
+  end
+
+  test "#invoice" do
+    get :invoice, format: :json, id: Transaction.first.id
+
+    transactions = JSON.parse(response.body, symbolize_names: true)
+    transaction  = transactions.first
+
+    assert_response :success
+    assert_equal Hash, transactions.class
+  end
 end
